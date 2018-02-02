2722a2e255b3c1e38df00ae33d6169e7f27167e3
Jorddagreatest <31973637+Jorddagreatest@users.noreply.github.com>
2018-02-01 20:02:57 -0700

    Add files via upload

---
+++ calculator functions.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    if (a != null && b != null)
+    return a + b;
+}
+
+function subtract(a, b) {
+    if (a != null && b != null)
+    return a - b;
+}
+
+function multiply(a, b){
+    if (a != null && b != null)
+    return a * b;
+}
+
+function divide(a, b){
+    if (a != null && b != null)
+    return a / b;
+}
